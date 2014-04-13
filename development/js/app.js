f15033d8cae22949ea5e956daaefcfd754a6b7c7
Ivan Pauletti <ivanpauletti@gmail.com>
2014-04-13 18:01:09 -0300

    Angular Routes (and first route and view, diary.html)

--- development/js/app.js
+++ development/js/app.js
@@ -8,15 +8,7 @@
 **/
 
 
-/**
- * Controller CarCtrl
- * Description: Creates a car
- * Methods:
- */
-
-
-'use strict';
-var LifeModule = angular.module("LifeModule", ['ngRoute']);
+var LifeModule = angular.module('LifeModule', ['ngRoute']);
 
 
 /**
@@ -35,11 +27,15 @@ LifeModule.config(['$routeProvider',
                 templateUrl: 'views/diary.html',
                 controller: 'DiaryCtrl'
             }).
-         otherwise({
-         templateUrl: 'views/diary.html',
-         controller: 'DiaryCtrl'
+            when('/dashboard/weight', {
+                templateUrl: 'views/weight.html',
+                controller: 'WeightCtrl'
+            }).
+             otherwise({
+                redirectTo: '/'
          });
-    }]);
+    }
+]);
 
 
 /**
@@ -49,127 +45,6 @@ LifeModule.config(['$routeProvider',
 LifeModule.config(['$locationProvider', function($locationProvider){
     $locationProvider.html5Mode(false).hashPrefix('!');
 }]);
-
-
-/**
- * Cria session interceptor para requisições HTTP, que controla validade da sessão entre requisições
- */
-LifeModule.factory("$sessionInterceptor", function ($q, $window, $log) {
-    var redirect = function () {
-        var loginLocation = LOGIN_PAGE;
-
-        // adiciona parâmetros da url no redirecionamento
-        var currentLocation = window.location.href;
-        if (/\?/.test(currentLocation)) {
-            loginLocation += '?' + currentLocation.split('?')[1];
-        }
-        $window.location = loginLocation;
-    };
-    var isLogged = false;
-
-    return {
-        request: function (config) {
-            var token = $window.localStorage["dt.admin.token"];
-            var lastRequestTime = $window.localStorage["dt.admin.lastRequestTime"];
-
-            var now = new Date().getTime();
-            // verifica se último request faz mais de 10 minutos
-            // (caso request seja para operação de login ignora validação)
-            if (/Login$/.test(config.url) || token && lastRequestTime && (now - lastRequestTime) < 10 * 60 * 1000) {
-                // logado, continuando requisição
-                isLogged = true;
-                return config || $q.when(config);
-            } else {
-                $log.info("$sessionInterceptor => Sessão local expirada, último request a mais de 10 minutos, preparando redirecionamento...");
-                $window.localStorage.removeItem("dt.admin.token");
-
-                // tratamento para quando estiver no login.html e evitar loop.
-                if ((window['LOGIN_PAGE'] != undefined) || (window['LOGIN_PAGE'] != void 0)) {
-                    $log.info("$sessionInterceptor =>  Ok, redirecionando para login.html");
-                    redirect();
-                    return $q.reject(config);
-                } else {
-                    $log.info("$sessionInterceptor =>  Já está no login.html. Redirecionamento cancelado!");
-                    return config || $q.when(config);
-                }
-            }
-        },
-        response: function (response) {
-            // verifica a presença do header "dt-legacy-http-status" no header da resposta
-            var dtLegacyHttpStatus = response.headers("dt-legacy-http-status");
-            if (dtLegacyHttpStatus) {
-                // converte para numero inteiro
-                dtLegacyHttpStatus = parseInt(dtLegacyHttpStatus, 10);
-
-                // verifica se existe o código 403, se existir sessão foi expirada, redirecionando p/ login
-                if (dtLegacyHttpStatus === 403) {
-                    $window.localStorage.removeItem("dt.admin.token");
-                    redirect();
-                    return $q.reject(config);
-
-                } else if (Modernizr.localstorage) {
-                    $window.localStorage["dt.admin.lastRequestTime"] = new Date().getTime();
-                }
-            } else {
-                if (Modernizr.localstorage) {
-                    $window.localStorage["dt.admin.lastRequestTime"] = new Date().getTime();
-                } else {
-                    $log.error("$sessionInterceptor => No localStorage, impossível armazenar último request...");
-                }
-            }
-
-            return response || $q.when(response);
-        }
-    };
-});
-
-/**
- * Cria interceptor que loga todas as requisições
- */
-LifeModule.factory("$httpLogInterceptor", function ($q, $log) {
-    return {
-        request: function (config) {
-            $log.info("$httpLogInterceptor.request => " + config.url);
-            return config || $q.when(config);
-        },
-        requestError: function (rejection) {
-            $log.info("$httpLogInterceptor.requestError => " + rejection);
-            return $q.reject(rejection);
-        },
-        response: function (response) {
-            $log.info("$httpLogInterceptor.response => " + response.config.url + " STATUS: " + response.status);
-            return response || $q.when(response);
-        },
-        responseError: function (rejection) {
-            $log.info("$httpLogInterceptor.responseError => " + rejection);
-            return $q.reject(rejection);
-        }
-    };
-});
-
-/**
- * Interceptador de erros, envia para o $errorService as requisições com status de erro
- */
-LifeModule.factory("$responseErrorInterceptor", function ($q, $errorService) {
-    return {
-        responseError: function (rejection) {
-            $errorService.addHttpError(rejection.status, rejection.config);
-            return $q.reject(rejection);
-        }
-    };
-});
-
-
-// Registra interceptors no módulo admin
-LifeModule.config(function ($httpProvider) {
-    //$httpProvider.interceptors.push('$sessionInterceptor');
-    $httpProvider.interceptors.push('$responseErrorInterceptor');
-    if ($.url(window.location.href).param("debug")) {
-        $httpProvider.interceptors.push('$httpLogInterceptor');
-    }
-});
-
-
 
 /**
  * Inits
---
+++ development/js/controllers/diary.js
@@ -0,0 +1,14 @@
+/**
+ * Controller  Diary
+ * @param $scope
+ * @param $http
+ * @param $log
+ * @param $windows
+ * @param $errorService
+ * @constructor
+ */
+LifeModule.controller('DiaryCtrl', function ($scope) {
+
+    alert("este");
+
+});
